9f8bdf5ada2e568706d75f4e272b8b18ee355600
zoar <460891016@qq.com>
2020-03-26 03:11:26 +0800

    json

--- json.v
+++ json.v
@@ -1,23 +1,20 @@
 module main
 
-fn main() {
-	str:='
-	{
-		"adsd":-253543,
-		"arr":[
-			{
-				"tt":"283,:[]2{9}741","show":false}
-		],
-		"hhh":{
-			"name":"zhan[gsan"
-		}
-	}'
-
-	parse(str)
-}
+__global token_index int
+__global tokens []Token
 
 enum TokenType {
-	null number str  begin_object end_object begin_array end_array boolean colon comma docment
+	null//null
+	number//-,0-9
+	str//"
+	begin_object //{
+	end_object //}
+	begin_array //[
+	end_array //]
+	boolean //f,t
+	colon //:
+	comma //,
+	docment
 } 
 
 struct Token{
@@ -25,68 +22,107 @@ struct Token{
 	value string
 }
 
+fn main() {
+	str:='
+	{
+		"adsd":-253.543,"arr":[
+			{
+				"tt":"283,:[]2{9}741","show":false}
+		],
+		"sd":"s21"
+	}'
+
+	parse(str)
+}
+
 fn parse(str string) {
 	new_str:= str.trim_space()
-	mut result:=[]Token
+	tokens = []
 	for i:=0; i<new_str.len;i++ {
 		s:=new_str[i].str().trim_space()
 		if s=='"' {
 			temp:= new_str.substr(i+1,new_str.index_after('"',i+1))
-			i += temp.len+1
-			tt:= TokenType{
-				token_type:TokenType.str,
+			tokens << Token{
+				token_type:.str,
 				value:temp
 			}
-			result << tt
+			i += temp.len+1
 		}
-		else if s in '{' {
-			result << TokenType{
-				token_type:TokenType.begin_object,
+		else if s == '{' {
+			tokens << Token{
+				token_type:.begin_object,
 				value:s
 			}
 		}
-		else if s in '}' {
-			result << TokenType{
-				token_type:TokenType.end_object,
+		else if s == '}' {
+			tokens << Token{
+				token_type:.end_object,
 				value:s
 			}
 		}
-		else if s in '[' {
-			result << TokenType{
-				token_type:TokenType.begin_array,
+		else if s == '[' {
+			tokens << Token{
+				token_type:.begin_array,
 				value:s
 			}
 		}
-		else if s in ['{','}','[',']',',',':'] {
-			result << s
+		else if s == ']' {
+			tokens << Token{
+				token_type:.end_array,
+				value:s
+			}
 		}
-		else if s=='f' {
-			result << parse_constont_value(i,'false',new_str)
+		else if s == ',' {
+			tokens << Token{
+				token_type:.comma,
+				value:s
+			}
+		}
+		else if s == ':' {
+			tokens << Token{
+				token_type:.colon,
+				value:s
+			}
+		}
+		else if s == 'f'{
+			tokens << Token{
+				token_type:.boolean,
+				value:parse_constont_value(i,'false',new_str)
+			}
 			i+=4
 		}
-		else if s=='t' {
-			result << parse_constont_value(i,'true',new_str)
+		else if s == 't' {
+			tokens << Token{
+				token_type:.boolean,
+				value:parse_constont_value(i,'true',new_str)
+			}
 			i+=3
 		}
 		else if s=='n' {
-			result << parse_constont_value(i,'null',new_str)
+			tokens << Token{
+				token_type:.null,
+				value:parse_constont_value(i,'null',new_str)
+			}
 			i+=3
 		}
 		else if s in ['-','0','1','2','3','4','5','6','7','8','9'] {
 			mut j:=i+1
 			for ;; j++ {
-				if ! new_str[j].str() in ['0','1','2','3','4','5','6','7','8','9']{
+				if ! new_str[j].str() in ['0','1','2','3','4','5','6','7','8','9','.']{
 					break
 				}
 			}
-			result << new_str.substr(i,j)
+			tokens << Token{
+				token_type:.boolean,
+				value:new_str.substr(i,j)
+			}
 			i=j-1
 		}
 		else {
 
 		}
 	}
-	println(result)
+	parse_json()
 }
 
 fn parse_constont_value(start int ,target string, sourse string) string {
@@ -96,4 +132,98 @@ fn parse_constont_value(start int ,target string, sourse string) string {
 	}else{
 		panic('json err  $temp')
 	}
+}
+fn check_expect_token(token Token,expect []TokenType) bool{
+	for item in expect {
+		if token.token_type== item{
+			return true
+		} 
+	}
+	return false
+}
+
+__global result voidptr 
+
+fn parse_json() {
+	token_index =0
+	println('tokens.len $tokens.len')
+	// temp := tokens[0]
+	// match temp.token_type {
+	// 	.begin_object {
+			parse_json_object() 
+		// }
+		// .begin_array {
+		// 	parse_json_array()
+		// }
+		// else{
+		// 	panic('json format err')
+		// }
+	// }
+}
+
+
+fn parse_json_object() ?map[string]voidptr {
+	mut obj:=map[string]voidptr
+	mut expect_tokens := [ TokenType.str , .end_object]
+	mut key:=''
+	for i := token_index; i < tokens.len;{
+		temp:= tokens[token_index]
+		
+		if check_expect_token(temp,expect_tokens) {
+			println('for $token_index == $temp.value')
+			match temp.token_type {
+				.begin_object {
+					v:=parse_json_object() 
+					obj[key]=&v
+					expect_tokens=[.str,.end_object]
+				}
+				.end_object {
+					return obj
+				}
+				.begin_array {
+					parse_json_array()
+				}
+				.str {
+					next:= tokens[token_index]
+					if next.token_type ==.colon{
+						v :=temp.value.clone()
+						obj[key]=&v
+						expect_tokens=[.comma,.end_object]
+						println('value = $key : $v')
+					}else{
+						key =temp.value
+						expect_tokens=[.colon]
+					}
+				}
+				.number {
+					next:= tokens[token_index]
+					if next.token_type ==.colon{
+						v :=temp.value.clone()
+						obj[key]=&v
+						expect_tokens=[.comma,.end_object]
+						println('value = $key : $v')
+					}else{
+						key =temp.value
+						expect_tokens=[.colon]
+					}
+				}
+				.colon {
+					expect_tokens=[ .str , .end_object , .begin_array , .number , .null , .boolean ]
+				}
+				.colon {
+					expect_tokens=[ .str ]
+				}
+				else {
+
+				}
+			}
+		}else{
+			//error
+		}
+		token_index++
+		i = token_index
+	}
+	return obj
+}
+fn parse_json_array() {
 }
